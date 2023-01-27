0f53b2923789b70153771569a6def3e1b87ebecc
Talha <73413298+TalhaAmjad0034@users.noreply.github.com>
2023-01-27 17:09:21 +0500

    make balls interactive, random size, random color, canvas size generation

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
 
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+let maxRadius = 40;
+
+let colorArray = ["#2C3E50", "#E74C3C", "#ECF0F1", "#3498DB", "#2980B9"];
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+});
+
 function Circle(x, y, dx, dy, radius) {
+  let minRadius = radius;
+  let color = colorArray[Math.floor(Math.random() * colorArray.length)];
   this.draw = function () {
     ctx.beginPath();
-    ctx.arc(x, y, 30, 0, Math.PI * 2, false);
-    ctx.strokeStyle = "blue";
-    ctx.stroke();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = color;
     ctx.fill();
   };
   this.update = function () {
@@ -25,6 +46,21 @@ function Circle(x, y, dx, dy, radius) {
     x += dx;
     y += dy;
 
+    // interactivity
+
+    if (
+      mouse.x - x < 50 &&
+      mouse.x - x > -50 &&
+      mouse.y - y < 50 &&
+      mouse.y - y > -50
+    ) {
+      if (radius < maxRadius) {
+        radius += 1;
+      }
+    } else if (radius > minRadius) {
+      radius -= 1;
+    }
+
     this.draw();
   };
 }
@@ -33,16 +69,28 @@ function Circle(x, y, dx, dy, radius) {
 
 let circleArr = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 400; i++) {
   var x = Math.random() * (innerWidth - radius * 2) + radius;
   var y = Math.random() * (innerHeight - radius * 2) + radius;
   var dx = Math.random() - 0.5;
   var dy = Math.random() - 0.5;
-  var radius = 30;
+  var radius = Math.random() * 10 + 1;
   circleArr.push(new Circle(x, y, dx, dy, radius));
 }
 
-console.log(circleArr);
+function init() {
+  // empty the array
+  circleArr = [];
+  // create new circles on new window size
+  for (let i = 0; i < 400; i++) {
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = Math.random() - 0.5;
+    var dy = Math.random() - 0.5;
+    var radius = Math.random() * 10 + 1;
+    circleArr.push(new Circle(x, y, dx, dy, radius));
+  }
+}
 
 function animate() {
   requestAnimationFrame(animate);
